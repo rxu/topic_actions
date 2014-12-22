d81495756b3db68a11b8ff43742fcbe07f7a9b6f
rxu <rxu@mail.ru>
2014-12-22 22:10:44 +0700

    Update French translation file's header.

--- language/fr/topic_actions.php
+++ language/fr/topic_actions.php
@@ -1,11 +1,10 @@
 <?php
 /** 
 *
-* topic_actions [French]
+* Topic Actions extension for the phpBB Forum Software package.
 *
-* @package language
-* @copyright (c) 2014 Ruslan Uzdenov (rxu)
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 * French translation by Galixte (http://www.galixte.com)
 *
