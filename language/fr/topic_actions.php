dad9f946940bd67496e64dc0ece452d3de1122a3
Raphaël M <Galixte@users.noreply.github.com>
2017-01-22 08:48:14 +0100

    Update of the FRENCH translation for Topic actions schedule extension

--- language/fr/topic_actions.php
+++ language/fr/topic_actions.php
@@ -1,12 +1,11 @@
 <?php
 /**
 *
-* Topic Actions extension for the phpBB Forum Software package.
-*
-* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
+* Topic actions schedule extension for the phpBB Forum Software package.
 * French translation by Galixte (http://www.galixte.com)
+*
+* @copyright (c) 2015 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
 
@@ -34,47 +33,42 @@ if (empty($lang) || !is_array($lang))
 // You do not need this where single placeholders are used, e.g. 'Message %d' is fine
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
 
 $lang = array_merge($lang, array(
-	'SCHEDULE_TOPIC_ACTION'			=> 'Schedule topic action',
-	'SELECT_ACTION'					=> 'Select action',
-	'DELETE_ACTION'					=> 'Delete action',
-	'TOPIC_ACTION_ERROR'			=> 'The requested action could not be performed.',
-	'TOPIC_ACTION_PERFORMED'		=> 'The requested action successfully completed.',
-	'TOPIC_ACTION_NO_PERMISSION'	=> 'You do not have correct permissions to perform the requested action.',
-	'TOPIC_ACTION_SET'				=> 'Action time was set successfully.',
-	'TOPIC_ACTION_DELETED'			=> 'Action time was deleted successfully.',
-	'NO_ACTION_SELECTED'			=> 'No action was selected.',
-	'NO_TIME_SET'					=> 'Action was not planned.',
+	'SCHEDULE_TOPIC_ACTION'			=> 'Planifier l’action sur le sujet',
+	'SELECT_ACTION'					=> 'Sélectionner',
+	'DELETE_ACTION'					=> 'Supprimer',
+	'TOPIC_ACTION_ERROR'			=> 'L’action demandée n’a pu être réalisée.',
+	'TOPIC_ACTION_PERFORMED'		=> 'L’action demandée a pu être réalisée avec succès.',
+	'TOPIC_ACTION_NO_PERMISSION'	=> 'Afin de réaliser l’action demandée il est nécessaire d’avoir les permissions adéquates.',
+	'TOPIC_ACTION_SET'				=> 'Temps d’action paramétré avec succès.',
+	'TOPIC_ACTION_DELETED'			=> 'Temps d’action supprimé avec succès.',
+	'NO_ACTION_SELECTED'			=> 'Aucune action sélectionnée.',
+	'NO_TIME_SET'					=> 'L’action n’a pas été planifiée.',
 	'TOPIC_ACTION'	=> array(
 		'NOT_ENOUGH_PARAMS'	=> 'Pas assez de paramètres.',
-		'NO_ACTION_SELECTED'=> 'Aucune action sélectionnée.',
-		'NO_TIME_SET'		=> 'Action non planifiée.',
 		'DELAY'				=> 'Délais de l’action.',
 		'DELAY_EXPLAIN'		=> 'Action planifiée sur le sujet : %1$s le %2$s',
-		'SELECT_ACTION'		=> 'Sélectionner l’action',
-		'DELETE_ACTION'		=> 'Supprimer l’action',
-		'TOPIC_ACTION_PERFORMED'=> 'L’action planifiée a été exécutée avec succès.',
-		'TOPIC_ACTION_SET'	=> 'La planification de l’action a été paramétrée avec succès.',
-		'TOPIC_ACTION_DELETED'	=> 'La planification de l’action a été supprimée avec succès.',
 		'TIME'	=> array(
-			'0'		=> 'Maintenant',
-			'1'		=> 'dans un jour',
-			'3'		=> 'dans trois jours',
-			'5'		=> 'dans cinq jours',
-			'7' 	=> 'dans une semaine',
-			'14'	=> 'dans deux semaines',
-			'30'	=> 'dans un mois'
+			'0'  => 'Maintenant',
+			'1'  =>  'dans un jour',
+			'3'  => 'dans trois jours',
+			'5'  => 'dans cinq jours',
+			'7'  => 'dans une semaine',
+			'14'  => 'dans deux semaines',
+			'30'  => 'dans un mois'
 		),
 		'TYPE'	=> array(
-			'trash'			=> 'Supprimer (avec restauration possible)',
-			'trash_lock'	=> 'Verrouiller puis supprimer (avec restauration possible)',
+			'trash'			=> 'Supprimer (restauration possible)',
+			'trash_lock'	=> 'Verrouiller puis supprimer (restauration possible)',
 			'delete'		=> 'Supprimer définitivement',
-			'lock'			=> 'Lock',
-			'unlock'		=> 'Unlock',
+			'lock'			=> 'Verrouiller',
+			'unlock'		=> 'Déverrouiller',
 			/*			'FORK'			=> 'Copier',
-						'LOCK'			=> 'Verrouiller',
-						'UNLOCK'		=> 'Déverrouiller',
 						'CHANGE_TYPE'	=> array(
 							'MAKE_ANNOUNCE'	=> 'Créer une annonce',
 							'MAKE_GLOBAL'	=> 'Créer une annonce globale',
@@ -84,11 +78,11 @@ $lang = array_merge($lang, array(
 			*/
 		),
 		'TYPE_NOTICE'	=> array(
-			'trash'			=> 'soft deleted',
-			'trash_lock'	=> 'soft deleted',
-			'delete'		=> 'deleted',
-			'lock'			=> 'locked',
-			'unlock'		=> 'unlocked',
+			'trash'			=> 'supprimé (restauration possible)',
+			'trash_lock'	=> 'verrouillé puis supprimer (restauration possible)',
+			'delete'		=> 'supprimé',
+			'lock'			=> 'verrouillé',
+			'unlock'		=> 'déverrouillé',
 		),
 	),
 ));
